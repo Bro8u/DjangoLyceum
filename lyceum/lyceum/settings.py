434213f4b782cc04b510f63a9014c6e35a410fab
Boris <betttttt@yandex.ru>
2024-10-01 23:43:07 +0300

    hw1

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    "beautifull"
+    "fake"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
