263c95e5d16c8574b1db1da8780bbf4f8082c35f
Boris <betttttt@yandex.ru>
2024-10-02 14:35:23 +0300

    black

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -95,13 +95,19 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "MinimumLengthValidator"), # noqa: E501
+        "NAME": (
+            "django.contrib.auth.password_validation." "MinimumLengthValidator"
+        ),  # noqa: E501
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "CommonPasswordValidator"), # noqa: E501
+        "NAME": (
+            "django.contrib.auth.password_validation." "CommonPasswordValidator"
+        ),  # noqa: E501
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "NumericPasswordValidator"), # noqa: E501
+        "NAME": (
+            "django.contrib.auth.password_validation." "NumericPasswordValidator"
+        ),  # noqa: E501
     },
 ]
 
