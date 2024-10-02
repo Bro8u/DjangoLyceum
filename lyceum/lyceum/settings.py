ff1cdf52cffb582b06f5a5683559622afc699238
Boris <betttttt@yandex.ru>
2024-10-02 13:54:29 +0300

    safe

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
