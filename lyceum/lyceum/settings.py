b0c4eee412fdb775bf159b46310da17e1d1b3865
Boris <betttttt@yandex.ru>
2024-10-11 17:54:20 +0300

    Week 2 HomeWork 1(Fix mistakes3)
    Fix dev tool is not installed

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -9,11 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "abba")
 
-DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+DEBUG_ENV = os.getenv("DJANGO_DEBUG", "True").lower()
+DEBUG = DEBUG_ENV in ("true", "yes", "1", "y", "t")
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(
-    ","
-)
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 
 INSTALLED_APPS = [
