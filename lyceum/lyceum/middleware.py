9909a3b6d0dbb68b3ef9c8f8c62e8399cc47794f
Boris <betttttt@yandex.ru>
2024-10-13 21:35:37 +0300

    week2 hw 3(fix class variable count_responses)

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 
 class ReverseWordsMiddleware:
+    count_responses = 0
 
     def __init__(self, get_response):
         self.get_response = get_response
-        self.count_responses = 0
 
     def __call__(self, request):
         response = self.get_response(request)
